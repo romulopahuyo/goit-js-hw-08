ad497e1ef4f0585b595a967d5c314bdc510ef314
romulopahuyo <romulo.pahuyo@gmail.com>
2024-05-05 16:36:11 +0800

    RevA

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -29,7 +29,7 @@ import SimpleLightbox from 'simplelightbox';
 // Additional styles import
 
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items';
 // Change code below this line
 
 // console.log(galleryItems);
